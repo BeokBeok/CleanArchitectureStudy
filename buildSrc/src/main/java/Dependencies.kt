b7a61a8f053562219850325205306f4a0afae151
BeokBeok <kekemusa37@nate.com>
2019-11-06 11:32:03 +0900

    버전 업데이트 (0.0.1 -> 0.0.2)
    - Crash 이슈 수정
    NoSuchMethodError

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -26,8 +26,8 @@ object App {
 }
 
 object Release {
-    const val VERSION_CODE = 1
-    const val VERSION_NAME = "0.0.1"
+    const val VERSION_CODE = 2
+    const val VERSION_NAME = "0.0.2"
 }
 
 object Test {
