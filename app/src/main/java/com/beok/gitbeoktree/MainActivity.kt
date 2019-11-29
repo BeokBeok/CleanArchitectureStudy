7f70e7c1ef67698c20fc5e7ae28b7e27f8624ae7
BeokBeok <kekemusa37@nate.com>
2019-11-29 23:54:56 +0900

    전면 광고 추가

--- app/src/main/java/com/beok/gitbeoktree/MainActivity.kt
+++ app/src/main/java/com/beok/gitbeoktree/MainActivity.kt
@@ -1,14 +1,6 @@
 package com.beok.gitbeoktree
 
-import android.os.Bundle
 import com.beok.common.base.BaseActivity
 import com.beok.gitbeoktree.databinding.ActivityMainBinding
-import com.google.android.gms.ads.MobileAds
 
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        MobileAds.initialize(this, getString(R.string.app_key_admob))
-    }
-}
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main)
